7b322311aa0da3bab25086195b0829c0df7a9eb6
Andrey Kalashnikov <kalashnikow@mail.ru>
2018-11-20 17:32:56 +0300

    Все приведено к общему виду для горизонтального выравнивания

--- js/stat.js
+++ js/stat.js
@@ -1,46 +1,82 @@
 'use strict';
 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_SHADOW = 10;
+var CLOUD_RADIUS = 75;
+var COLUMN_WIDTH = 40;
+var COLUMN_INTERVAL = 50;
+var COLUMN_MAX_HEIGHT = 150;
+
+/*
+Прямоугольник с закругленными углами, начиная с левого верхнего угла по часовой стрелке
+Ширина CLOUD_WIDTH
+Высота CLOUD_HEIGHT
+Левый верхний угол CLOUD_X  CLOUD_Y
+Радиус скругления CLOUD_RADIUS
+Отступ тени CLOUD_RADIUS
+*/
+
+/* x, y -левый верхний угол */
+var renderCloud = function (ctx, x ,y, color) {
+  ctx.fillStyle = color;
+  var pi = Math.PI;
+  ctx.beginPath();
+  ctx.arc(CLOUD_RADIUS + x, CLOUD_RADIUS + y, CLOUD_RADIUS, pi, pi * 3 / 2);
+  ctx.lineTo(CLOUD_WIDTH - CLOUD_RADIUS + x, 0 + y);
+  ctx.arc(CLOUD_WIDTH - CLOUD_RADIUS + x, CLOUD_RADIUS + y, CLOUD_RADIUS, pi * 3 / 2, pi * 2);
+  ctx.lineTo(CLOUD_WIDTH + x, CLOUD_HEIGHT - CLOUD_RADIUS + y);
+  ctx.arc(CLOUD_WIDTH - CLOUD_RADIUS + x, CLOUD_HEIGHT - CLOUD_RADIUS + y, CLOUD_RADIUS, 0, pi / 2);
+  ctx.lineTo(CLOUD_RADIUS + x, CLOUD_HEIGHT + y);
+  ctx.arc(CLOUD_RADIUS + x, CLOUD_HEIGHT - CLOUD_RADIUS + y, CLOUD_RADIUS, pi / 2, pi);
+  ctx.lineTo(0 + x, CLOUD_RADIUS + y);
+  ctx.fill();
+};
+
+var getMaxNumber = function (numbers) {
+  var maxNumber = numbers[0];
+  for (var i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
+    };
+  };
+  return maxNumber;
+};
+
 var renderStatistics = function (ctx, names, times) {
 
   var columnHeights = [];
 
-  /*Облако*/
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, 420, 270);
+  /*Тень и облако*/
+  renderCloud(ctx, CLOUD_X + CLOUD_SHADOW, CLOUD_Y + CLOUD_SHADOW, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgb(255,255,255)');
 
   /*Текст*/
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', 230, 30);
+  ctx.fillText('Ура, вы победили!', 230, 30); /* Текст отцентрирован для ширины облака 420 */
   ctx.fillText('Список результатов:', 220, 50);
 
-  /*Имена*/
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], 155 + 90 * i, 270);
-  };
+  var maxTime = getMaxNumber(times);
 
-  /*Столбцы*/
-  /*Максимальное время*/
-  var maxTime = times[0];
-  for (var i = 1; i < times.length; i++) {
-    if (times[i] > maxTime) {
-      maxTime = times[i];
+  var count = names.length; /* Количество столбцов */
+  for (var i = 0; i < count; i++) {
+    var color = 'rgb(0, 0, 255, '+ Math.random() + ')';
+    if (names[i] === 'Вы') {
+      color = 'rgba(255, 0, 0, 1)';
     };
+    ctx.fillStyle = color;
+
+    var column_start_x = CLOUD_X + (CLOUD_WIDTH - count * COLUMN_WIDTH - (count - 1) * COLUMN_INTERVAL) / 2 + i * (COLUMN_WIDTH + COLUMN_INTERVAL);
+    var columnHeight = Math.round(times[i] * (COLUMN_MAX_HEIGHT / maxTime));
+    ctx.fillText(names[i], column_start_x, 270);
+    ctx.fillText(Math.round(times[i]), column_start_x, 230 - columnHeight);
+    ctx.fillRect(column_start_x, 245 - columnHeight, COLUMN_WIDTH, columnHeight);
   };
 
-  /*Высоты столбцов*/
-  for (var i = 0; i < times.length; i++) {
-    columnHeights[i] = Math.round(times[i] * (150 / maxTime));
-  };
 
-  /*Рисуем столбцы и их значения*/
-  for (var i = 0; i < columnHeights.length; i++) {
-    names[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgb(0, 0, 255, '+ Math.random() + ')';
-    ctx.fillText(Math.round(times[i]), 155 + 90 * i, 230 - columnHeights[i]);
-    ctx.fillRect(155 + 90 * i, 245 - columnHeights[i], 40, columnHeights[i]);
-  };
 
 };
 
