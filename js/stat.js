e80da79a9bee8ad19f86de655f03750549c0a2ab
Andrey Kalashnikov <kalashnikow@mail.ru>
2018-11-21 12:19:15 +0300

    Отцентрировал текст по горизонтали

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,7 @@ var CLOUD_RADIUS = 75;
 var COLUMN_WIDTH = 40;
 var COLUMN_INTERVAL = 50;
 var COLUMN_MAX_HEIGHT = 150;
+var GAP = 20;
 
 /*
 Прямоугольник с закругленными углами, начиная с левого верхнего угла по часовой стрелке
@@ -20,7 +21,7 @@ var COLUMN_MAX_HEIGHT = 150;
 */
 
 /* x, y -левый верхний угол */
-var renderCloud = function (ctx, x ,y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   var pi = Math.PI;
   ctx.beginPath();
@@ -40,41 +41,42 @@ var getMaxNumber = function (numbers) {
   for (var i = 1; i < numbers.length; i++) {
     if (numbers[i] > maxNumber) {
       maxNumber = numbers[i];
-    };
-  };
+    }
+  }
   return maxNumber;
 };
 
 var renderStatistics = function (ctx, names, times) {
 
-  /*Тень и облако*/
+  /* Тень и облако */
   renderCloud(ctx, CLOUD_X + CLOUD_SHADOW, CLOUD_Y + CLOUD_SHADOW, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgb(255,255,255)');
 
-  /*Текст*/
+  /* Текст */
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', 230, 30); /* Текст отцентрирован для ширины облака 420 */
-  ctx.fillText('Список результатов:', 220, 50);
+  ctx.textAlign = 'center';
+  ctx.fillText('Ура, вы победили!', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP * 2);
 
   var maxTime = getMaxNumber(times);
 
+  ctx.textBaseline = 'hanging';
+
   var count = names.length; /* Количество столбцов */
   for (var i = 0; i < count; i++) {
-    var color = 'rgb(0, 0, 255, '+ Math.random() + ')';
+    var color = 'rgb(0, 0, 255, ' + Math.random() + ')';
     if (names[i] === 'Вы') {
       color = 'rgba(255, 0, 0, 1)';
-    };
+    }
     ctx.fillStyle = color;
 
     var columnStartX = CLOUD_X + (CLOUD_WIDTH - count * COLUMN_WIDTH - (count - 1) * COLUMN_INTERVAL) / 2 + i * (COLUMN_WIDTH + COLUMN_INTERVAL);
     var columnHeight = Math.round(times[i] * (COLUMN_MAX_HEIGHT / maxTime));
-    ctx.fillText(names[i], columnStartX, 270);
-    ctx.fillText(Math.round(times[i]), columnStartX, 230 - columnHeight);
-    ctx.fillRect(columnStartX, 245 - columnHeight, COLUMN_WIDTH, columnHeight);
-  };
-
-
+    ctx.fillText(names[i], columnStartX + COLUMN_WIDTH / 2, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), columnStartX + COLUMN_WIDTH / 2, CLOUD_Y + CLOUD_HEIGHT - GAP * 2.5 - columnHeight);
+    ctx.fillRect(columnStartX, CLOUD_Y + CLOUD_HEIGHT - GAP * 1.5 - columnHeight, COLUMN_WIDTH, columnHeight);
+  }
 
 };
 
