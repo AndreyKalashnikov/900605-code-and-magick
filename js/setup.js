79bed82dfde13aff44f95eb636b2bfe75f59afcf
Andrey Kalashnikov <kalashnikow@mail.ru>
2018-11-22 16:51:39 +0300

    Первая версия третьего задания

---
+++ js/setup.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var wizards = [];
+
+var getRandomFromDiaposon = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+
+for (var j = 0; j < 4; j++) {
+  wizards.push({
+    name: NAMES[getRandomFromDiaposon(0, NAMES.length - 1)] + ' ' + SURNAMES[getRandomFromDiaposon(0, SURNAMES.length - 1)],
+    coatColor: COAT_COLORS[getRandomFromDiaposon(0, COAT_COLORS.length - 1)],
+    eyesColor: EYES_COLORS[getRandomFromDiaposon(0, EYES_COLORS.length - 1)]
+  });
+
+  var newWizard = renderWizard(wizards[j]);
+  fragment.appendChild(newWizard);
+}
+
+similarListElement.appendChild(fragment);
